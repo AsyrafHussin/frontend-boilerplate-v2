943cc39c97ad9a9b4b11e1669db811530735216e
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-09-16 20:14:12 +0800

    Update import jquery

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,10 @@
 // Global Import
-import jquery from 'jquery';
-window.jQuery = jquery;
-window.$ = jquery;
-window.jquery = jquery;
+window.jQuery = window.$ = require('jquery');
 import 'bootstrap';
 
 // import module
 import Scroll from './modules/scroll.js';
 
-$(document).ready(function() {
-  Scroll.init();
+jQuery(function () {
+    Scroll.init();
 });
